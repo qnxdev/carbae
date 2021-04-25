0c706f06db2281ba2a35ade226880426d34274af
Dev <you@example.com>
2021-04-25 14:28:19 +0530

    API fetch & Listing

---
+++ src/components/CarItem.jsx
@@ -0,0 +1,21 @@
+export const CarItem = ({ car }) => {
+  const varientCount =
+    (car.transmission_types === "both" ? 2 : 0) +
+    (car.fuel_types === "both" ? 2 : 1);
+
+  return (
+    <div key={car.id} className="listItem">
+      <img src={car.main_image} alt="Not available" />
+      <div className="carDetail">
+        <p>{car.name}</p>
+        <h4>
+          &#8377;
+          {` ${Math.floor(car.price_starts / 1000) / 100} - ${
+            Math.floor(car.price_ends / 1000) / 100
+          } Lakh`}
+        </h4>
+        <p>{`${varientCount} varient${varientCount !== 1 ? "s" : ""}`}</p>
+      </div>
+    </div>
+  );
+};
--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { CarItem } from "./CarItem";
+
 export default function Listing() {
-    
+  const [cars, setCars] = useState([]);
+
+  const GetCars = async () => {
+    const resp = await fetch(
+      `https://6083a8f35dbd2c001757b9a7.mockapi.io/api/v1/cars`
+    );
+    setCars(await resp.json());
+  };
+
+  useEffect(() => {
+    if (cars && cars.length === 0) {
+      GetCars();
+    }
+  }, [cars.length]);
+
+  if (cars && cars.length <= 0) {
+    return "Loading";
+  } else {
+    return (
+      <div className="listing">
+        {cars.map((car) => (
+          <CarItem car={car} />
+        ))}
+      </div>
+    );
+  }
 }
