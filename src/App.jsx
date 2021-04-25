a5f2b49942fbf81caf12f26cccae30e18791392a
Dev <you@example.com>
2021-04-25 14:30:15 +0530

    Redux Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Listing from './components/Listing';
 
 function App() {
   return (
     <div className="App">
+      <Listing/>
     </div>
   );
 }
---
+++ src/actions/carAction.js
@@ -0,0 +1,6 @@
+export default function carsAction(payload){
+    return {
+        type: 'cars',
+        payload
+    }
+}
